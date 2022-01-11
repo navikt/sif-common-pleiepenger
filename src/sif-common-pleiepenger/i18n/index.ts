857e16d19d7ba02fc407ba38039204bac378ffd6
Frode Hansen <frode.hansen@bekk.no>
2022-01-11 08:43:34 +0100

    Legge til flere messages filer default

--- src/sif-common-pleiepenger/i18n/index.ts
+++ src/sif-common-pleiepenger/i18n/index.ts
@@ -1,5 +1,8 @@
+import { omsorgstilbudEnkeltdagFormMessages } from '..';
+import arbeidstidEnkeltdagFormMessages from '../arbeidstid-enkeltdag/arbeidstidEnkeltdagMessages';
 import { arbeidstidPeriodeMessages } from '../arbeidstid-periode/arbeidstidPeriodeMessages';
 import { omsorgstibudPeriodeMessages } from '../omsorgstilbud-periode/omsorgstilbudPeriodeMessages';
+import tidEnkeltdagFormMessages from '../tid-enkeltdag-dialog/tidEnkeltdagMessages';
 import { timerOgMinutterMessages } from '../timer-og-minutter/timerOgMinutterMessages';
 
 export type ComponentMessages<Messages> = Record<string, Messages>;
@@ -13,6 +16,9 @@ export const sifCommonPleiepengerMessages = {
     nb: {
         ...arbeidstidPeriodeMessages.nb,
         ...omsorgstibudPeriodeMessages.nb,
+        ...omsorgstilbudEnkeltdagFormMessages.nb,
         ...timerOgMinutterMessages.nb,
+        ...tidEnkeltdagFormMessages.nb,
+        ...arbeidstidEnkeltdagFormMessages.nb,
     },
 };
