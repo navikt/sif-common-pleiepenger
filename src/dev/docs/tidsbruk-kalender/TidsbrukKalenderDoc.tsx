87fd2e55e3889b6c524f8abff4d45300c08a53da
Frode Hansen <frode.hansen@bekk.no>
2022-01-24 09:52:30 +0100

    TidsbrukKalender doc

---
+++ src/dev/docs/tidsbruk-kalender/TidsbrukKalenderDoc.tsx
@@ -0,0 +1,72 @@
+import { DateRange, ISODateToDate } from '@navikt/sif-common-utils/lib';
+import React from 'react';
+import { TidsbrukKalender } from '../../../sif-common-pleiepenger';
+import TidArbeidEnkeltdag from '../../../sif-common-pleiepenger/arbeidstid-måned-info/TidArbeidEnkeltdag';
+import PageIntro from '../../components/page-intro/PageIntro';
+
+const måned: DateRange = {
+    from: ISODateToDate('2021-01-01'),
+    to: ISODateToDate('2020-01-31'),
+};
+
+// const datesInMonth = getDatesInMonth(måned.from);
+
+const TidsbrukKalenderDoc = () => {
+    return (
+        <>
+            <PageIntro title="@navikt/sif-common-pleiepenger">
+                <h2>TidsbrukKalender</h2>
+                <TidsbrukKalender
+                    dager={{
+                        '2021-01-01': {
+                            hours: '2',
+                            minutes: '0',
+                        },
+                        '2021-01-04': {
+                            hours: '2',
+                            minutes: '0',
+                        },
+                        '2021-01-05': {
+                            hours: '2',
+                            minutes: '0',
+                        },
+                    }}
+                    dagerOpprinnelig={{
+                        '2021-01-01': {
+                            hours: '7',
+                            minutes: '30',
+                        },
+                        '2021-01-04': {
+                            hours: '7',
+                            minutes: '30',
+                        },
+                        '2021-01-05': {
+                            hours: '5',
+                            minutes: '30',
+                        },
+                    }}
+                    utilgjengeligeDatoer={[
+                        ISODateToDate('2021-01-06'),
+                        ISODateToDate('2021-01-07'),
+                        ISODateToDate('2021-01-11'),
+                        ISODateToDate('2021-01-12'),
+                        ISODateToDate('2021-01-13'),
+                        ISODateToDate('2021-01-14'),
+                        ISODateToDate('2021-01-15'),
+                    ]}
+                    tomUkeContentRenderer={() => <>Ingen dager tilgjengelig denne uken</>}
+                    periode={måned}
+                    skjulTommeDagerIListe={true}
+                    visEndringsinformasjon={true}
+                    tidRenderer={({ tid, prosent }) => <TidArbeidEnkeltdag tid={tid} prosent={prosent} />}
+                    opprinneligTidRenderer={({ tid, prosent }) => <TidArbeidEnkeltdag tid={tid} prosent={prosent} />}
+                    onDateClick={(date: Date) => {
+                        console.log(date);
+                    }}
+                />
+            </PageIntro>
+        </>
+    );
+};
+
+export default TidsbrukKalenderDoc;
