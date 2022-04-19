d964cfc3e547fd26638dc4480a551c557b79e6e9
Frode Hansen <frode.hansen@bekk.no>
2022-04-19 07:34:49 +0200

    Fikse bem import

--- src/sif-common-pleiepenger/label-input-info-layout/LabelInputInfoLayout.tsx
+++ src/sif-common-pleiepenger/label-input-info-layout/LabelInputInfoLayout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import bemUtils from '../../dev/utils/bemUtils';
+import bemUtils from '@navikt/sif-common-core/lib/utils/bemUtils';
 import './labelInputInfoLayout.less';
 
 interface Props {
