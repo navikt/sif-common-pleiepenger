c9ce3e48f55a967731cdfb539b3bafd3ce4b6e89
Frode Hansen <frode.hansen@bekk.no>
2022-04-21 11:16:05 +0200

    Utvide default exports

--- src/sif-common-pleiepenger/index.ts
+++ src/sif-common-pleiepenger/index.ts
@@ -2,21 +2,26 @@ export { default as ArbeidstidEnkeltdagerListe } from './arbeidstid/arbeidstid-e
 export { default as ArbeidstidFasteDagerListe } from './arbeidstid/arbeidstid-faste-dager-liste/ArbeidstidFasteDagerListe';
 export { default as ArbeidstidEnkeltdagDialog } from './arbeidstid/arbeidstid-enkeltdag-dialog/ArbeidstidEnkeltdagDialog';
 export { default as arbeidstidEnkeltdagMessage } from './arbeidstid/arbeidstid-enkeltdag-dialog/arbeidstidEnkeltdagMessages';
+export { default as ArbeidstidMåned } from './arbeidstid/arbeidstid-måned/ArbeidstidMåned';
 export { default as ArbeidstidPeriodeDialog } from './arbeidstid/arbeidstid-periode/components/arbeidstid-periode-dialog/ArbeidstidPeriodeDialog';
 export { default as ArbeidstidPeriodeForm } from './arbeidstid/arbeidstid-periode/components/arbeidstid-periode-form/ArbeidstidPeriodeForm';
+export type { ArbeidstidPeriodeFormProps } from './arbeidstid/arbeidstid-periode/components/arbeidstid-periode-form/ArbeidstidPeriodeForm';
 export { default as ArbeidstidUkeInput } from './arbeidstid/arbeidstid-uke-input/ArbeidstidUkeInput';
+export type { ArbeidstidUkeInputEnkeltdagValidator } from './arbeidstid/arbeidstid-uke-input/ArbeidstidUkeInput';
 export { default as ArbeidstidUkerInput } from './arbeidstid/arbeidstid-uker-input/ArbeidstidUkerInput';
 
 export { default as OmsorgstilbudEnkeltdagDialog } from './omsorgstilbud/omsorgstilbud-enkeltdag/OmsorgstilbudEnkeltdagDialog';
 export { default as omsorgstilbudEnkeltdagFormMessages } from './omsorgstilbud/omsorgstilbud-enkeltdag/omsorgstilbudEnkeltdagFormMessages';
 export { default as OmsorgstilbudPeriodeDialog } from './omsorgstilbud/omsorgstilbud-periode/components/omsorgstilbud-periode-dialog/OmsorgstilbudPeriodeDialog';
 export { default as OmsorgstilbudPeriodeForm } from './omsorgstilbud/omsorgstilbud-periode/components/omsorgstilbud-periode-form/OmsorgstilbudPeriodeForm';
+export type { OmsorgstilbudPeriodeData } from './omsorgstilbud/omsorgstilbud-periode/components/omsorgstilbud-periode-form/OmsorgstilbudPeriodeForm';
 export { default as OmsorgstilbudMåned } from './omsorgstilbud/omsorgstilbud-periode/OmsorgstilbudMåned';
 
 export { default as TidEnkeltdager } from './tid/dager-med-tid/TidEnkeltdager';
 export { default as TidFasteDager } from './tid/dager-med-tid/TidFasteDager';
 export { default as TidEnkeltdagDialog } from './tid/tid-enkeltdag-dialog/TidEnkeltdagDialog';
 export { default as TidEnkeltdagForm } from './tid/tid-enkeltdag-dialog/TidEnkeltdagForm';
+export type { TidEnkeltdagEndring } from './tid/tid-enkeltdag-dialog/TidEnkeltdagForm';
 export { default as TidFasteUkedagerInput } from './tid/tid-faste-ukedager-input/TidFasteUkedagerInput';
 export { default as TidKalenderForm } from './tid/tid-kalender-form/TidKalenderForm';
 export { default as TidUkeInput } from './tid/tid-uke-input/TidUkeInput';
@@ -35,4 +40,5 @@ export type { TidRenderer } from './tid/tidsbruk-kalender/TidsbrukKalender';
 export * from './types';
 export * from './arbeidstid/arbeidstid-periode/utils/arbeidstidPeriodeUtils';
 export * from './arbeidstid/arbeidstid-periode/components/arbeidstid-periode-form/arbeidstidPeriodeFormValidation';
+export * from './omsorgstilbud/omsorgstilbud-periode/components/omsorgstilbud-periode-form/omsorgstilbudFormValidation';
 import './styles/styles.less';
