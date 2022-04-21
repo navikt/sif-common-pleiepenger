c91abb973881e3a1fed359e812a2c7627db5e159
Frode Hansen <frode.hansen@bekk.no>
2022-04-21 10:59:31 +0200

    Default exports

--- src/sif-common-pleiepenger/index.ts
+++ src/sif-common-pleiepenger/index.ts
@@ -1,22 +1,36 @@
+export { default as ArbeidstidEnkeltdagerListe } from './arbeidstid/arbeidstid-enkeltdager-liste/ArbeidstidEnkeltdagerListe';
+export { default as ArbeidstidFasteDagerListe } from './arbeidstid/arbeidstid-faste-dager-liste/ArbeidstidFasteDagerListe';
 export { default as ArbeidstidEnkeltdagDialog } from './arbeidstid/arbeidstid-enkeltdag-dialog/ArbeidstidEnkeltdagDialog';
-export { GjentagelseType } from './tid/tid-enkeltdag-dialog/TidEnkeltdagForm';
-export type { TidEnkeltdagFormValues, GjentagelseEnkeltdag } from './tid/tid-enkeltdag-dialog/TidEnkeltdagForm';
 export { default as arbeidstidEnkeltdagMessage } from './arbeidstid/arbeidstid-enkeltdag-dialog/arbeidstidEnkeltdagMessages';
 export { default as ArbeidstidPeriodeDialog } from './arbeidstid/arbeidstid-periode/components/arbeidstid-periode-dialog/ArbeidstidPeriodeDialog';
-export { default as DurationText } from './common/duration-text/DurationText';
+export { default as ArbeidstidPeriodeForm } from './arbeidstid/arbeidstid-periode/components/arbeidstid-periode-form/ArbeidstidPeriodeForm';
+export { default as ArbeidstidUkeInput } from './arbeidstid/arbeidstid-uke-input/ArbeidstidUkeInput';
+export { default as ArbeidstidUkerInput } from './arbeidstid/arbeidstid-uker-input/ArbeidstidUkerInput';
+
 export { default as OmsorgstilbudEnkeltdagDialog } from './omsorgstilbud/omsorgstilbud-enkeltdag/OmsorgstilbudEnkeltdagDialog';
 export { default as omsorgstilbudEnkeltdagFormMessages } from './omsorgstilbud/omsorgstilbud-enkeltdag/omsorgstilbudEnkeltdagFormMessages';
 export { default as OmsorgstilbudPeriodeDialog } from './omsorgstilbud/omsorgstilbud-periode/components/omsorgstilbud-periode-dialog/OmsorgstilbudPeriodeDialog';
+export { default as OmsorgstilbudPeriodeForm } from './omsorgstilbud/omsorgstilbud-periode/components/omsorgstilbud-periode-form/OmsorgstilbudPeriodeForm';
 export { default as OmsorgstilbudMåned } from './omsorgstilbud/omsorgstilbud-periode/OmsorgstilbudMåned';
-export { default as SøknadsperioderMånedListe } from './common/søknadsperioder-måned-liste/SøknadsperioderMånedListe';
-export { default as CalendarGrid } from './common/calendar-grid/CalendarGrid';
-export { default as TidEnkeltdager } from './depr/dager-med-tid/TidEnkeltdager';
-export { default as TidFasteDager } from './depr/dager-med-tid/TidFasteDager';
-export { default as TidKalenderForm } from './tid/tid-kalender-form/TidKalenderForm';
-export { default as TidUkerInput } from './tid/tid-uker-input/TidUkerInput';
+
+export { default as TidEnkeltdager } from './tid/dager-med-tid/TidEnkeltdager';
+export { default as TidFasteDager } from './tid/dager-med-tid/TidFasteDager';
+export { default as TidEnkeltdagDialog } from './tid/tid-enkeltdag-dialog/TidEnkeltdagDialog';
+export { default as TidEnkeltdagForm } from './tid/tid-enkeltdag-dialog/TidEnkeltdagForm';
 export { default as TidFasteUkedagerInput } from './tid/tid-faste-ukedager-input/TidFasteUkedagerInput';
+export { default as TidKalenderForm } from './tid/tid-kalender-form/TidKalenderForm';
+export { default as TidUkeInput } from './tid/tid-uke-input/TidUkeInput';
+export { default as TidUkerInput } from './tid/tid-uker-input/TidUkerInput';
 export { default as TidsbrukKalender } from './tid/tidsbruk-kalender/TidsbrukKalender';
+export { GjentagelseType } from './tid/tid-enkeltdag-dialog/TidEnkeltdagForm';
+export type { TidEnkeltdagFormValues, GjentagelseEnkeltdag } from './tid/tid-enkeltdag-dialog/TidEnkeltdagForm';
+
+export { default as DurationText } from './common/duration-text/DurationText';
+export { default as CalendarGrid } from './common/calendar-grid/CalendarGrid';
+export { default as LabelInputInfoLayout } from './common/label-input-info-layout/LabelInputInfoLayout';
+export { default as SøknadsperioderMånedListe } from './common/søknadsperioder-måned-liste/SøknadsperioderMånedListe';
 export { default as TimerOgMinutter, formatTimerOgMinutter } from './common/timer-og-minutter/TimerOgMinutter';
+
 export type { TidRenderer } from './tid/tidsbruk-kalender/TidsbrukKalender';
 export * from './types';
 export * from './arbeidstid/arbeidstid-periode/utils/arbeidstidPeriodeUtils';
