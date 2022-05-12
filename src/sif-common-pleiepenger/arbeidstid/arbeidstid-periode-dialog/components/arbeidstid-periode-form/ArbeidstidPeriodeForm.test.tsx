555ab914890c88949d411ba0291dfbc75dd0bb49
Frode Hansen <frode.hansen@bekk.no>
2022-05-12 09:01:26 +0200

    Fikse build av lib

--- src/sif-common-pleiepenger/arbeidstid/arbeidstid-periode-dialog/components/arbeidstid-periode-form/ArbeidstidPeriodeForm.test.tsx
+++ src/sif-common-pleiepenger/arbeidstid/arbeidstid-periode-dialog/components/arbeidstid-periode-form/ArbeidstidPeriodeForm.test.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from '@storybook/testing-react';
-import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import dayjs from 'dayjs';
